aa6b9096075b785a88b20185cae92c076102b37f
zhanghanbo <557451@qq.com>
2024-07-13 18:44:43 +0800

    make it a python package

--- evaluate_calvin.sh
+++ evaluate_calvin.sh
@@ -2,10 +2,10 @@
 
 export MESA_GL_VERSION_OVERRIDE=3.3
 
-python3 evaluate_calvin.py \
+python3 gr1/evaluate_calvin.py \
     --eval_dir ${EVAL_DIR} \
     --mae_ckpt_path ${MAE_CKPT_PATH} \
     --policy_ckpt_path ${POLICY_CKPT_PATH} \
-    --configs_path logs/configs.json \
-    --dataset_dir task_ABCD_D/ \
+    --configs_path gr1/logs/configs.json \
+    --dataset_dir gr1/task_ABCD_D/ \
     ${@:1}
